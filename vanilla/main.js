6fae2d7a98c9ca16c2ebd5a40ffe9e9c9a995b19
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-12-07 10:30:38 +0900

    Editor#getBody and Editor#getDoc

--- vanilla/main.js
+++ vanilla/main.js
@@ -4,10 +4,13 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello TinyMCE!!</h1>
 `
 
-const editor = await tinymce.init({
+const editors = await tinymce.init({
   selector: '#mytextarea'
 })
 
 document.getElementById('mybutton').addEventListener('click', () => {
-  console.log(editor[0].getContent());
+  const editor = editors[0]; // editor is only one
+  console.log('getBody', editor.getBody());
+  console.log('getContent', editor.getContent());
+  console.log('getDoc', editor.getDoc());
 })
