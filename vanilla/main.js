19f541a0a5327279b39776012c70327057c9e46d
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-12-07 11:45:13 +0900

    Editor#setContent

--- vanilla/main.js
+++ vanilla/main.js
@@ -6,15 +6,17 @@ document.querySelector('#app').innerHTML = `
 
 /**
  * TODO
- * 1. Editor#setContent
- * 2. Table plugin
+ * 1. Table plugin
  */
 
 let editor;
 tinymce.init({
   selector: '#mytextarea',
   language: 'ja',
-}).then(editors => editor = editors[0]) // editor is only one
+}).then(editors => {
+  editor = editors[0] // editor is only one
+  editor.setContent('<p>Edit <strong>me</strong>!</p>')
+})
 
 document.getElementById('mybutton').addEventListener('click', () => {
   console.log('getBody', editor.getBody());
