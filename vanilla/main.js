a6263d6a682b2b1f0c5e04c61e1f8ae968255039
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-12-07 11:55:01 +0900

    table plugin

--- vanilla/main.js
+++ vanilla/main.js
@@ -3,16 +3,12 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <h1>Hello TinyMCE!!</h1>
 `
-
-/**
- * TODO
- * 1. Table plugin
- */
-
 let editor;
 tinymce.init({
   selector: '#mytextarea',
   language: 'ja',
+  plugins: 'table',
+  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol'
 }).then(editors => {
   editor = editors[0] // editor is only one
   editor.setContent('<p>Edit <strong>me</strong>!</p>')
