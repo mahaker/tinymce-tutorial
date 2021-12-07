5007677894b1f23dd424f0e6b71c960f02371056
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-12-07 15:43:40 +0900

    setup tinymce-vue
    https://www.tiny.cloud/docs/integrations/vue/

---
+++ tinymce-vue-offical-component/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Editor from '@tinymce/tinymce-vue';
+</script>
+
+<template>
+  <div id="app">
+    <section>
+      <h1>hello world!</h1>
+    </section>
+    <section>
+      <editor
+        :init="{
+          height: 500,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help'
+        }"
+      />
+    </section>
+  </div>
+</template>
